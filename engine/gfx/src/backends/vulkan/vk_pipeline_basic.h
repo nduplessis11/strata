791cc3ef9428de6300161f7b61b19694a4cb5468
Nick DuPlessis <nduplessis11@users.noreply.github.com>
2025-11-27 13:12:04 -0500

    Include Vulkan header in basic pipeline

--- engine/gfx/src/backends/vulkan/vk_pipeline_basic.h
+++ engine/gfx/src/backends/vulkan/vk_pipeline_basic.h
@@ -8,32 +8,7 @@
 
 #pragma once
 
-#include <cstdint>
-
-// Forward declarations to avoid pulling Vulkan headers into dependent code.
-// These mirror the canonical Vulkan definitions:
-//   - Handles are opaque pointers to incomplete types.
-//   - VkExtent2D is declared but defined by the Vulkan headers; we only need a reference.
-struct VkDevice_T;
-struct VkPipelineLayout_T;
-struct VkPipeline_T;
-
-using VkDevice = VkDevice_T*;
-using VkPipelineLayout = VkPipelineLayout_T*;
-using VkPipeline = VkPipeline_T*;
-
-// Value types: declare the *tags* only, no fake typedefs:
-struct VkExtent2D;  // matches Vulkan's "struct VkExtent2D { ... }"
-
-// MSVC whines about this, so silence that one warning only:
-#ifdef _MSC_VER
-#  pragma warning(push)
-#  pragma warning(disable : 4471) // forward declaration of unscoped enum must have an underlying type
-#endif
-enum VkFormat; // Vulkan defines: "typedef enum VkFormat { ... } VkFormat;"
-#ifdef _MSC_VER
-#  pragma warning(pop)
-#endif
+#include <vulkan/vulkan.h>
 
 namespace strata::gfx::vk {
 
